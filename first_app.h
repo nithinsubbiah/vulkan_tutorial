12977eb3284804979b37c38fc94b98155e11117b
Nithin <nithinsubbiah@gmail.com>
2023-01-24 19:27:04 -0800

    display a triangle

--- first_app.h
+++ first_app.h
@@ -3,22 +3,34 @@
 #include "lve_device.h"
 #include "lve_pipeline.h"
 #include "lve_window.h"
+#include "lve_swap_chain.h"
+
+#include <memory>
 
 namespace lve {
 
 class FirstApp {
   public:
-    void run();
     static constexpr int WIDTH = 800;
     static constexpr int HEIGHT = 600;
+    
+    FirstApp();
+    ~FirstApp();
 
+    FirstApp(const FirstApp &) = delete;
+    FirstApp &operator=(const FirstApp &) = delete;
+    void run();
   private:
+    void createPipelineLayout();
+    void createPipeline();
+    void createCommandBuffers();
+    void drawFrame();
+
     LveWindow lveWindow{WIDTH, HEIGHT, "Hello World!"};
     LveDevice lveDevice{lveWindow};
-    LvePipeline lvePipeline{
-        lveDevice,
-        "shaders/simple_shader.vert.spv",
-        "shaders/simple_shader.frag.spv",
-        LvePipeline::defaultPipelineConfigInfo(WIDTH, HEIGHT)};  
+    LveSwapChain lveSwapChain{lveDevice, lveWindow.getExtent()};
+    std::unique_ptr<LvePipeline> lvePipeline;
+    VkPipelineLayout pipelineLayout;
+    std::vector<VkCommandBuffer> commandBuffers;
 };
 } // namespace lve
